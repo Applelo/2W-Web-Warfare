bf043a5a46111101da863556fefa703df462cb08
SarT0x <quantin.sole@orange.fr>
2017-03-08 17:19:24 +0100

    *Change game.html

--- www/js/game.js
+++ www/js/game.js
@@ -2,9 +2,11 @@ $( function() {
   $( "#draggable" ).draggable();
     $( "#draggable2" ).draggable();
       $( "#draggable3" ).draggable();
+
   $( "#droppable" ).droppable({
     drop: function( event, ui ) {
-      $( this ).css('background', 'purple');
+      $("#droppable").css('background', 'purple');
     }
   });
+
 } );
