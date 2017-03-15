87fb317b047dd4c34368832e42372dbd5ed5783d
SarT0x <quantin.sole@orange.fr>
2017-03-15 10:29:11 +0100

    *Update Interact 1.2.8

--- www/js/game.js
+++ www/js/game.js
@@ -42,7 +42,7 @@ interact('.draggable')
 
   interact('.dropzone').dropzone({
     // only accept elements matching this CSS selector
-    //accept: '#yes-drop',
+    accept: '#drag-1',
     // Require a 75% element overlap for a drop to be possible
     overlap: 0.75,
 
@@ -61,12 +61,13 @@ interact('.draggable')
       draggableElement.classList.add('can-drop');
       draggableElement.textContent = 'Dragged in';
     },
-    ondragleave: function (event) {
+    /*ondragleave: function (event) {
       // remove the drop feedback style
       event.target.classList.remove('drop-target');
       event.relatedTarget.classList.remove('can-drop');
       event.relatedTarget.textContent = 'Dragged out';
     },
+    */
     ondrop: function (event) {
       event.relatedTarget.textContent = 'Dropped';
     },
