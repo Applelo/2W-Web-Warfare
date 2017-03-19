ab19ac94fe9f5e713636a7eea996863a593c4fdd
SarT0x <quantin.sole@orange.fr>
2017-03-19 16:56:33 +0100

    *Reconnaissance des cartes OK

--- www/js/game.js
+++ www/js/game.js
@@ -1,24 +1,29 @@
 // target elements with the "draggable" class
 interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: false,
-    // keep the element within the area of it's parent
-    restrict: {
-      restriction: "parent",
-      endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-    // enable autoScroll
-    autoScroll: true,
+    .draggable({
+        // enable inertial throwing
+        inertia: false,
+        // keep the element within the area of it's parent
+        restrict: {
+            restriction: "#outer-dropzone",
+            endOnly: true,
+            elementRect: {
+                top: 0,
+                left: 0,
+                bottom: 1,
+                right: 1
+            }
+        },
+        // enable autoScroll
+        autoScroll: true,
 
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-    // call this function on every dragend event
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+        // call this function on every dragend event
 
-  });
+    });
 
-  function dragMoveListener (event) {
+function dragMoveListener(event) {
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -26,31 +31,41 @@ interact('.draggable')
 
     // translate the element
     target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
 
     // update the posiion attributes
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
-  }
+}
 
-  // this is used later in the resizing and gesture demos
-  window.dragMoveListener = dragMoveListener;
+function nameClick() {
+    nameDrag = this.id;
+    console.log(nameDrag);
+}
+
+var inputs = document.getElementsByTagName("img");
+for (var i = 0; i < inputs.length; i++) {
+    var nameTag = inputs[i].addEventListener("click", nameClick);
+}
 
 
-  //DROPZONE
-  interact('.dropzone').dropzone({
 
+
+//DROPZONE
+interact('.dropzone').dropzone({
     // only accept elements matching this CSS selector
-    accept: '#drag-1',
+    accept: nameDrag,
+
     // Require a 75% element overlap for a drop to be possible
     overlap: 0.75,
 
-    // ondragenter: function (event) {
-    //   var draggableElement = event.relatedTarget,
-    //       dropzoneElement = event.target;
-    // },
-    ondrop: function (event) {
-      document.getElementById("outer-dropzone").style.backgroundColor = "yellow";
+
+    ondrop: function(event) {
+          if (nameDrag == "#drag-1") {
+          document.getElementById("outer-dropzone").style.backgroundColor = "yellow";
+      }
+
     },
-  });
+
+});
