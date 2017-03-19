043265599c9842602336a7fe95f9f13e24728000
SarT0x <quantin.sole@orange.fr>
2017-03-19 14:39:22 +0100

    *Changement carte + plateau

--- www/js/game.js
+++ www/js/game.js
@@ -38,27 +38,19 @@ interact('.draggable')
   window.dragMoveListener = dragMoveListener;
 
 
-//DROPZONE
-
+  //DROPZONE
   interact('.dropzone').dropzone({
+
     // only accept elements matching this CSS selector
     accept: '#drag-1',
     // Require a 75% element overlap for a drop to be possible
     overlap: 0.75,
 
-    ondragenter: function (event) {
-      var draggableElement = event.relatedTarget,
-          dropzoneElement = event.target;
-
-    },
-
+    // ondragenter: function (event) {
+    //   var draggableElement = event.relatedTarget,
+    //       dropzoneElement = event.target;
+    // },
     ondrop: function (event) {
       document.getElementById("outer-dropzone").style.backgroundColor = "yellow";
-      document.getElementById("outer-dropzone").innerHTML = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mi metus, porta at ullamcorper vel, tincidunt at velit. Aliquam consequat quam non arcu rutrum, a luctus ligula sollicitudin </p>';
     },
-    ondropdeactivate: function (event) {
-      // remove active dropzone feedback
-      event.target.classList.remove('drop-active');
-      event.target.classList.remove('drop-target');
-    }
   });
