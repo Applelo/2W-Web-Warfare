fde1cd151d190decad8bca3350735998b9d9c06c
Applelo <boubaultlois@gmail.com>
2017-03-20 16:49:59 +0100

    *add card-function basic

--- www/js/card-css.js
+++ www/js/card-css.js
@@ -1,3 +1,34 @@
+/*background : change le background d'un élément*/
 function CSSbackground() {
   document.getElementById("outer-dropzone").style.backgroundColor = "yellow";
+}
+
+/*text-align : aligne le texte à droite*/
+function CSStextalign() {
+
+}
+
+/*color : change la couleur des éléments en rouge*/
+function CSScolor() {
+
+}
+
+/*text-decoration : souligne le texte*/
+function CSStext_decoration() {
+
+}
+
+/*display : change l'affichage des éléments*/
+function CSSdisplay() {
+
+}
+
+/*font-family : change la font */
+function CSSfont() {
+
+}
+
+/*position : change la position d'un élément */
+function CSSposition() {
+
 }
--- www/js/card-html.js
+++ www/js/card-html.js
@@ -1,3 +1,9 @@
+/*p : insère un paragraphe avec du lorem ipsum*/
+function HTMLa() {
+  document.getElementById("outer-dropzone").innerHTML = document.getElementById("outer-dropzone").innerHTML + "<a href='#'>Cliquez ici</a>";
+}
+
+/*p : insère un paragraphe avec du lorem ipsum*/
 function HTMLp() {
   document.getElementById("outer-dropzone").innerHTML = document.getElementById("outer-dropzone").innerHTML + "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis lorem vel feugiat finibus. Fusce eget pellentesque ipsum. Cras eget interdum erat, eu elementum eros. Aliquam non malesuada dui. Sed nulla tellus, tincidunt sit amet consequat eu, tempus dignissim velit. Pellentesque aliquam orci eget luctus lacinia. Proin tortor eros, scelerisque feugiat purus vitae, auctor fringilla libero.</p>";
 }
