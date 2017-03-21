0a83a0573a81df5f8c8bc599f75b47aae28ee9ab
SarT0x <quantin.sole@orange.fr>
2017-03-21 13:20:27 +0100

    *Add card css

--- www/js/card-css.js
+++ www/js/card-css.js
@@ -5,30 +5,30 @@ function CSSbackground() {
 
 /*text-align : aligne le texte à droite*/
 function CSStextalign() {
-
+  document.getElementById("outer-dropzone").style
 }
 
 /*color : change la couleur des éléments en rouge*/
 function CSScolor() {
-
+  document.getElementById("outer-dropzone").style.color = " red"
 }
 
 /*text-decoration : souligne le texte*/
 function CSStext_decoration() {
-
+  document.getElementById("outer-dropzone").
 }
 
 /*display : change l'affichage des éléments*/
 function CSSdisplay() {
-
+  document.getElementById("outer-dropzone").
 }
 
 /*font-family : change la font */
 function CSSfont() {
-
+  document.getElementById("outer-dropzone").
 }
 
 /*position : change la position d'un élément */
 function CSSposition() {
-
+  document.getElementById("outer-dropzone").
 }
